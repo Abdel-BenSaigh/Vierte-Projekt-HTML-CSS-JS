91a248c2238d6558f914a9420f52c5f38c4960ac
Abdel-BenSaigh <118136654+Abdel-BenSaigh@users.noreply.github.com>
2022-11-14 12:23:05 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let handyFoto = document.querySelector(".img")
+let backgrounContent = document.querySelector(".content")
+
+
+function handyFotos(iphonx) {
+    handyFoto.src = iphonx
+
+}
+
+function backgroundColor(color) {
+    backgrounContent.style.background = color
+}
